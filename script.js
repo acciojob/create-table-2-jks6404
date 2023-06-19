6b65badf8e704c46c9465bf58be9f7bd8d05d34c
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-06-19 12:20:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
-  
+  let rows=prompt('Input number of rows');
+  let columns=prompt('Input number of columns');
+  let table=document.getElementById('myTable');
+	table.innerHTML="";
+ for(let i=0;i<rows;i++){
+	 let row=document.createElement('tr');
+	 for(let j=0;j<columns;j++){
+		let col=document.createElement('td');
+		 col.innerText=`Row-${i} Column-${j}`;
+		 row.append(col);
+	 }
+	 table.append(row);
+ }
+	
 }
